9b35e365e90fa5334505737f6cd4ee5977c0b9c3
Hugo Sanchez <programacionhsv@gmail.com>
2019-05-18 23:11:25 -0500

    Configuraciones de aplicacion
    - Configuracion para remover index.php de la url
    - Configuracion de la base de datos
    - Configuracion de la ruta de prueba
    - Agregado bootstrap

--- application/config/database.php
+++ application/config/database.php
@@ -73,24 +73,4 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 $active_group = 'default';
 $query_builder = TRUE;
 
-$db['default'] = array(
-	'dsn'	=> '',
-	'hostname' => 'localhost',
-	'username' => '',
-	'password' => '',
-	'database' => '',
-	'dbdriver' => 'mysqli',
-	'dbprefix' => '',
-	'pconnect' => FALSE,
-	'db_debug' => (ENVIRONMENT !== 'production'),
-	'cache_on' => FALSE,
-	'cachedir' => '',
-	'char_set' => 'utf8',
-	'dbcollat' => 'utf8_general_ci',
-	'swap_pre' => '',
-	'encrypt' => FALSE,
-	'compress' => FALSE,
-	'stricton' => FALSE,
-	'failover' => array(),
-	'save_queries' => TRUE
-);
+$db['default']['dsn'] = "postgres://spevbdzi:TY79uTtQyHxPWQ63nK7vu8OCMvdCuQ1E@isilo.db.elephantsql.com:5432/spevbdzi";
---
+++ application/views/templates/header.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="es-MX">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/bootstrap.min.css">
+	<title>Cursos presenciales</title>
+</head>
+<body>
+	<nav class="navbar navbar-dark bg-primary">
+		<span class="navbar-brand mb-0 h1">Home</span>
+	</nav>
